692672c373b7642027d85df1e943373a384b20a2
AMixailV <Mixer-X@mail.ru>
2024-06-15 15:09:58 +0300

    Initial commit

---
+++ app/src/main/java/ru/mixail_akulov/a15_dialogfragment/MainActivity.kt
@@ -0,0 +1,28 @@
+package ru.mixail_akulov.a15_dialogfragment
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import ru.mixail_akulov.a15_dialogfragment.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater).also { setContentView(it.root) }
+
+        binding.level1Button.setOnClickListener {
+            startActivity(Intent(this, DialogsLevel1Activity::class.java))
+        }
+
+        binding.level2Button.setOnClickListener {
+            startActivity(Intent(this, DialogsLevel2Activity::class.java))
+        }
+
+        binding.exitButton.setOnClickListener {
+            finish()
+        }
+    }
+}
